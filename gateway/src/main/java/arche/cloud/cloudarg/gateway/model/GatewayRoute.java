1ef4c9b7c5a5ef6314d4f6c7d504efa371fa5621
Cloudbeer <cloudbeer@gmail.com>
2021-12-07 23:17:50 +0800

    delete all

--- gateway/src/main/java/arche/cloud/cloudarg/gateway/model/GatewayRoute.java
+++
@@ -1,8 +0,0 @@
-package arche.cloud.cloudarg.gateway.model;
-
-public class GatewayRoute {
-    private String path;
-    private String service;
-    private Integer targetPort;
-    private String targetPath;
-}
--- gateway/src/main/java/arche/cloud/cloudarg/gateway/model/RBAC.java
+++
@@ -1,7 +0,0 @@
-package arche.cloud.cloudarg.gateway.model;
-
-public class RBAC {
-    private String role;
-    private String type; //allow, forbid
-
-}
