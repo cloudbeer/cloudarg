8bdb2fcab472ccce0df5a716cf630398bcbbf7dd
Cloudbeer <cloudbeer@gmail.com>
2021-12-16 22:06:38 +0800

    build prepare

---
+++ test/2.sh
@@ -0,0 +1,6 @@
+curl -X GET  "http://47.114.92.162:8089/cloudarling/_/admin/account/list" \
+   -H "Authorization: bearer eyJvcGVuX2lkIjoiOWU4ZDVlMzMtOGIyMC00NTQ0LWIxYTAtMmVjMTY2MWJkNTU2IiwiZXhwaXJlc19pbiI6MTE1NjYyMDgyMzAwLCJzaWduIjoiMmJjOWI0YmI5YjgxOGU2YzE2MGYwMDNkNDc1NGI4MjFmMTJjODM1OTJhNmVmMDNlNmQ5NTRhY2NhZjA0ZDI1ZCJ9" \
+
+echo "\n--------\n"
+curl -X GET  "http://47.114.92.162:8089/cloudarling/_/admin/role/list" \
+   -H "Authorization: bearer eyJvcGVuX2lkIjoiOWU4ZDVlMzMtOGIyMC00NTQ0LWIxYTAtMmVjMTY2MWJkNTU2IiwiZXhwaXJlc19pbiI6MTE1NjYyMDgyMzAwLCJzaWduIjoiMmJjOWI0YmI5YjgxOGU2YzE2MGYwMDNkNDc1NGI4MjFmMTJjODM1OTJhNmVmMDNlNmQ5NTRhY2NhZjA0ZDI1ZCJ9" \
